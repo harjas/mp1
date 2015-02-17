8ae8d195eb1184768abd7e113af2e7cacc7b9460
harjas <harjassinghglobal@gmail.com>
2015-02-16 23:45:44 -0600

    social media icons, multi grid layout, fontawesome

--- js/script.js
+++ js/script.js
@@ -1,16 +1,39 @@
-var divs = document.getElementsByTagName('div');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
-}
-
-function highlightThis(event) {
-    //event.stopPropagation();
+// var divs = document.getElementsByTagName('div');
+// for(var i=0; i<divs.length; i++) {
+//   divs[i].addEventListener("click", highlightThis);
   
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
-}
+//   divs[i].addEventListener("click", highlightThis, true);
+//   divs[i].addEventListener("click", highlightThis, false);
+// }
+
+// function highlightThis(event) {
+//     //event.stopPropagation();
+  
+//     var backgroundColor = this.style.backgroundColor;
+//     this.style.backgroundColor='yellow';
+//     alert(this.className);
+//     this.style.backgroundColor=backgroundColor;
+// }
+
+$(document).on("scroll",function(){
+    if($(document).scrollTop()>200){
+        $("header").removeClass("large-nav").addClass("small-nav");
+    } else{
+        $("header").removeClass("small-nav").addClass("large-nav");
+    }
+});
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
